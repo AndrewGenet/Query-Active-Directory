33a24fc196c85a507682a38698358eefd485b766
Andrew Genet <awg6@zips.uakron.edu>
2017-11-15 10:28:30 -0500

    Add project files.

---
+++ Active Directory Tool/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace Active_Directory_Tool
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.propertyCb = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.resultsLb = new System.Windows.Forms.ListBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.pathTxt = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // propertyCb
+            // 
+            this.propertyCb.FormattingEnabled = true;
+            this.propertyCb.Location = new System.Drawing.Point(29, 79);
+            this.propertyCb.Name = "propertyCb";
+            this.propertyCb.Size = new System.Drawing.Size(234, 21);
+            this.propertyCb.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(29, 177);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(234, 57);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Search";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // resultsLb
+            // 
+            this.resultsLb.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.resultsLb.FormattingEnabled = true;
+            this.resultsLb.Location = new System.Drawing.Point(3, 16);
+            this.resultsLb.Name = "resultsLb";
+            this.resultsLb.Size = new System.Drawing.Size(616, 539);
+            this.resultsLb.TabIndex = 5;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(29, 131);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(234, 21);
+            this.textBox1.TabIndex = 6;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.groupBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.pathTxt);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.propertyCb);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(293, 558);
+            this.groupBox1.TabIndex = 8;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Attribute";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.comboBox1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(6, 279);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(281, 94);
+            this.groupBox3.TabIndex = 12;
+            this.groupBox3.TabStop = false;
+            this.groupBox3.Text = "Filter";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(23, 19);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(234, 21);
+            this.comboBox1.TabIndex = 7;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(23, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(234, 25);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Filter";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(26, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Path: ";
+            // 
+            // pathTxt
+            // 
+            this.pathTxt.Location = new System.Drawing.Point(29, 44);
+            this.pathTxt.Name = "pathTxt";
+            this.pathTxt.Size = new System.Drawing.Size(234, 20);
+            this.pathTxt.TabIndex = 10;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(37, 13);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Value:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.resultsLb);
+            this.groupBox2.Location = new System.Drawing.Point(311, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(622, 558);
+            this.groupBox2.TabIndex = 9;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(945, 578);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Query AD";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox propertyCb;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ListBox resultsLb;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox pathTxt;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ Active Directory Tool/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.DirectoryServices;
+
+
+namespace Active_Directory_Tool
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            groupBox1.Dock = DockStyle.Left;
+            groupBox2.Dock = DockStyle.Fill;
+            propertyCb.Items.Add("employeeid");
+            propertyCb.SelectedItem = "employeeid";
+
+            // LDAP path
+            pathTxt.Text = "LDAP://DC=,DC=,DC=,DC=";
+        }
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            resultsLb.Items.Clear();
+            DirectoryEntry entry = new DirectoryEntry(pathTxt.Text);
+            DirectorySearcher search = new DirectorySearcher(entry);
+
+            //search by employeeID
+            search.Filter = "(" + propertyCb.SelectedItem + "=" + textBox1.Text.ToString() + ")";
+            
+            try
+            {
+                SearchResult result = search.FindOne();
+                foreach(string name in result.Properties.PropertyNames)
+                {
+                    //add items to filter combo box
+                    comboBox1.Items.Add(name.ToString());
+                    //add items to the results pane
+                    resultsLb.Items.Add(name + " : " + result.Properties[name.ToString()][0].ToString());
+                }
+            }
+            catch (Exception ex)
+            {
+                resultsLb.Items.Add(ex.ToString());
+            }
+        }
+
+
+        // basically the same thing with a clever way of adding only one entry to the listBox
+        private void button2_Click(object sender, EventArgs e)
+        {
+            resultsLb.Items.Clear();
+            DirectoryEntry entry = new DirectoryEntry(pathTxt.Text);
+            DirectorySearcher search = new DirectorySearcher(entry);
+
+            search.Filter = "(" + propertyCb.SelectedItem + "=" + textBox1.Text.ToString() + ")";
+
+            try
+            {
+                SearchResult result = search.FindOne();
+                foreach (string name in result.Properties.PropertyNames)
+                {
+                    if (comboBox1.SelectedItem.ToString() == name)
+                    {
+                        resultsLb.Items.Add(name + " : " + result.Properties[name.ToString()][0].ToString());
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                resultsLb.Items.Add(ex.ToString());
+            }
+        }
+    }
+}
